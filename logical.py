d3c2cc54a4050c023db4f48ac3c30870bde56438
briankiptoo <48515925+briankiptoo@users.noreply.github.com>
2022-03-25 20:42:37 +0300

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,8 @@
+name= input('enter your name: ')
+if len(name) <3:
+    print('names must be atleat 3 char')
+elif len(name) >50:
+    print("Too long you son of a bitch")
+else:
+    print('okay 😂')
+
